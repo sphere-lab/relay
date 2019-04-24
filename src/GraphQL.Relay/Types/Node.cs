1915be5b9a2077f315ebec0948eee92626026d5a
Andrey Ivanov <a.ivanov@spherelab.ru>
2019-04-24 17:45:14 +0300

    Added TryParseFromGlobalId method

--- src/GraphQL.Relay/Types/Node.cs
+++ src/GraphQL.Relay/Types/Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Panic.StringUtils;
 
 namespace GraphQL.Relay.Types
@@ -17,10 +18,37 @@ namespace GraphQL.Relay.Types
             return StringUtils.Base64Encode("{0}:{1}".ToFormat(name, id));
         }
 
+        public static bool TryParseFromGlobalId(string globalIdString, out GlobalId globalId)
+        {
+            globalId = null;
+
+            if (IsBase64String(globalIdString))
+            {
+                var parts = StringUtils.Base64Decode(globalIdString).Split(':');
+                if (parts.Length > 1)
+                {
+                    globalId = new GlobalId()
+                    {
+                        Type = parts[0],
+                        Id = parts[1]
+                    };
+                }
+            }
+
+            return globalId != null;
+        }
+
+        private static bool IsBase64String(string s)
+        {
+            s = s.Trim();
+            return (s.Length % 4 == 0) && Regex.IsMatch(s, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+        }
+
         public static GlobalId FromGlobalId(string globalId)
         {
             var parts = StringUtils.Base64Decode(globalId).Split(':');
-            return new GlobalId {
+            return new GlobalId
+            {
                 Type = parts[0],
                 Id = string.Join(":", parts.Skip(count: 1)),
             };
